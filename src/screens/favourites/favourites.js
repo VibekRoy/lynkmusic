ee355720b9733d84063e327301771497f83266c9
VibekRoy <97304965+VibekRoy@users.noreply.github.com>
2023-08-17 08:06:53 +0530

    Delete src directory

--- src/screens/favourites/favourites.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Favourites() {
-  return (
-    <div className='screen-container'>sfdsdfsdf</div>
-  )
-}
-
-export default Favourites
--- src/screens/feed/feed.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Feed() {
-  return (
-    <div className='screen-container'>Feed</div>
-  )
-}
-
-export default Feed
